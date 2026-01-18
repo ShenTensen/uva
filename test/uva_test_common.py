2dd962be3873379d1213ddb5ae7082a45f689c73
谌恩发 <shentensen@outlook.com>
2026-01-18 21:16:01 +0800

    修复通用测试脚本

--- test/uva_test_common.py
+++ test/uva_test_common.py
@@ -5,6 +5,7 @@
 
 import os
 import re
+import subprocess
 import sys
 
 IS_WINDOWS = os.name == 'nt'
@@ -46,11 +47,13 @@ class ProgramTest():
 def compare_unbounded(program_out, reference_out, filename):
   passed = True
   program_output_array = []
+  line_count = 0
   while True:
+    line_count = line_count + 1
     line1 = program_out.readline().rstrip().decode("utf-8", "namereplace")
     line2 = reference_out.readline().rstrip()
-    print('program: ' + line1)
-    print('referen: ' + line2)
+    print('program  (' + str(line_count) + '): ' + line1)
+    print('reference(' + str(line_count) + '): ' + line2)
     if line2 == "":
       break
     program_output_array.append(line1)
@@ -63,16 +66,16 @@ def compare_unbounded(program_out, reference_out, filename):
   return passed
 
 
-def run_test(script_dir, program_name, test_slug, output_type=None):
+def run_test(script_dir, problem_dir, program_name, test_slug, output_type=None):
   output_slug = test_slug
   if output_type:
     output_slug += "-" + output_type
-  # program_path = os.path.join(script_dir, program_name)
-  program_path = program_name;
-  input_path = os.path.join(script_dir, "program_input", "%s-input.txt" % test_slug)
-  output_path = os.path.join(script_dir, "program_output", "%s-output.txt" % output_slug)
-  reference_path = os.path.join(script_dir, "reference_output", "%s-output-ref.txt" % output_slug)
+  program_path = program_name
+  input_path = os.path.join(problem_dir, "program_input", "%s-input.txt" % test_slug)
+  output_path = os.path.join(problem_dir, "program_output", "%s-output.txt" % output_slug)
+  reference_path = os.path.join(problem_dir, "reference_output", "%s-output-ref.txt" % output_slug)
   test_runner = ProgramTest(script_dir, program_path, input_path, reference_path)
+  return test_runner(output_path)
 
 
 def run_tests(problem_dir, command):
@@ -84,5 +87,18 @@ def run_tests(problem_dir, command):
     if match:
       test_slug = match.group(1)
       inputs.append("%s" % test_slug)
+  tests_passed = 0
+  tests_failed = 0
   for input_filename in inputs:
-    run_test(script_dir, command, input_filename)
+    did_pass, fail_reason = False, "Unknown test error"
+    did_pass, fail_reason = run_test(script_dir, problem_dir, command, input_filename)
+    if did_pass:
+      print("\tPASSED test: %s" % input_filename)
+      tests_passed += 1
+    else:
+      print("\tFAILED test: %s (%s)" % (input_filename, fail_reason))
+      tests_failed += 1
+    if len(inputs) == 0:
+      print("There are no tests for %s!\n" % command)
+    else:
+      print("Passed %d/%d tests\n" % (tests_passed, (tests_passed + tests_failed)))
