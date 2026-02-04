b5bd0e4b783db6d342576a27456259a1ae899dfd
谌恩发 <shentensen@outlook.com>
2026-02-04 16:13:10 +0800

    先将输出保存到文件中，再逐行比较程序输出文件和参考输出文件

--- test/uva_test_common.py
+++ test/uva_test_common.py
@@ -7,6 +7,7 @@ import os
 import re
 import subprocess
 import sys
+from itertools import zip_longest
 
 IS_WINDOWS = os.name == 'nt'
 IS_LINUX = os.name == 'posix' and os.uname()[0] == 'Linux'
@@ -24,18 +25,12 @@ class ProgramTest():
     self.input_path = input_path
     self.trace_path = trace_path
 
-  def __call__(self, filename):
-    with open(self.input_path, 'r') as input_f:
+  def __call__(self, output_path):
+    with open(self.input_path, 'r') as input_f, open(output_path, 'w') as output_f:
       proc = subprocess.Popen([self.program_path],
-                              cwd=self.group_path, stdin=input_f, stdout=subprocess.PIPE)
-    try:
-      reference = open(self.trace_path)
-      passed = compare_unbounded(proc.stdout, reference, filename)
-    finally:
-      try:
-        os.kill(proc.pid, signal.SIGTERM)
-      except Exception as e:
-        pass
+                              cwd=self.group_path, stdin=input_f, stdout=output_f)
+      proc.wait()  # 等待进程完成
+      passed = compare_files(self.trace_path, output_path)
     if passed:
       print('Passed.')
       return (True, "Matched expected output")
@@ -44,26 +39,50 @@ class ProgramTest():
       return (False, "Did not match expected output")
 
 
-def compare_unbounded(program_out, reference_out, filename):
-  passed = True
-  program_output_array = []
-  line_count = 0
-  while True:
-    line_count = line_count + 1
-    line1 = program_out.readline().rstrip().decode("utf-8", "namereplace")
-    line2 = reference_out.readline().rstrip()
-    print('program  (' + str(line_count) + '): ' + line1)
-    print('reference(' + str(line_count) + '): ' + line2)
-    if line2 == "":
-      break
-    program_output_array.append(line1)
-    m = re.match(line2, line1)
-    if m == None or m.start() != 0 or m.end() != len(line2):
-      passed = False
-  with open(filename, "w") as program_output:
-    for line in program_output_array:
-      program_output.write(line + "\n")
-  return passed
+# AI生成后手动修改
+def compare_files(path1: str, path2: str, encoding: str = 'utf-8') -> bool:
+  """
+  逐行比较两个文本文件的内容。
+
+  参数:
+  - path1, path2: 文件路径
+  - encoding: 打开文件时使用的编码（默认 'utf-8'）
+
+  返回:
+  - True: 两个文件逐行完全相同（忽略行尾的换行符差异）
+  - False: 发现任意一行不同，或两文件行数不同
+  """
+  try:
+    with open(path1, 'r', encoding=encoding, errors='replace') as f1, \
+         open(path2, 'r', encoding=encoding, errors='replace') as f2:
+      line_no = 0
+      result = True
+      for a, b in zip_longest(f1, f2, fillvalue=None):
+        line_no += 1
+        if a is None:
+          print("line no: " + str(line_no))
+          print("Refernce line empty, but output not!")
+          return False
+        elif b is None:
+          print("line no: " + str(line_no))
+          print("Output line empty, but reference not!")
+          return False
+        else:
+          # 比较时去掉行尾的换行符差异（但保留其他空格）
+          a = a.rstrip('\r\n')
+          b = b.rstrip('\r\n')
+          if a != b:
+            print("line no: " + str(line_no))
+            print("reference line:")
+            print(a, end="]\n")
+            print("output line:")
+            print(b, end="]\n")
+            return False
+    return True
+  except FileNotFoundError:
+    return False
+  except OSError:
+    return False
 
 
 def run_test(script_dir, problem_dir, program_name, test_slug, output_type=None):
@@ -72,8 +91,8 @@ def run_test(script_dir, problem_dir, program_name, test_slug, output_type=None)
     output_slug += "-" + output_type
   program_path = program_name
   input_path = os.path.join(problem_dir, "program_input", "%s-input.txt" % test_slug)
-  output_path = os.path.join(problem_dir, "program_output", "%s-output.txt" % output_slug)
   reference_path = os.path.join(problem_dir, "reference_output", "%s-output-ref.txt" % output_slug)
+  output_path = os.path.join(problem_dir, "program_output", "%s-output.txt" % output_slug)
   test_runner = ProgramTest(script_dir, program_path, input_path, reference_path)
   return test_runner(output_path)
 
