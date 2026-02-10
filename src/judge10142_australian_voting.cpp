9fa9e7d49de860cbf6847d2958af6e4e8efa14c3
谌恩发 <shentensen@outlook.com>
2026-02-11 01:42:28 +0800

    重新实现Judge 10142实现，通过测试

--- src/judge10142_australian_voting.cpp
+++ src/judge10142_australian_voting.cpp
@@ -9,6 +9,7 @@
 #include <map>
 #include <numeric>
 #include <iostream>
+#include <set>
 #include <string>
 #include <sstream>
 #include <unordered_map>
@@ -25,10 +26,9 @@ using Cases = std::vector<OneCase>;
 Cases ReadInput();
 Results Process(const Cases& cases);
 void OutputResults(const Results& results);
-void OutputCases(const Cases& cases);  // 调试使用。
 
 int main() {
-  Cases voting_cases = ReadInput();  
+  Cases voting_cases = ReadInput();
   Results results = Process(voting_cases);
   OutputResults(results);
   return 0;
@@ -69,25 +69,7 @@ Cases ReadInput() {
   return voting_cases;
 }
 
-
-void OutputCases(const Cases& voting_cases) {
-  // 打印读取的内容，方便测试。
-  std::cout << "输入的数据：" << '\n';
-  for (auto one_case : voting_cases) {
-    for (auto name: one_case.first) {
-      std::cout << name << '\n';
-    }
-
-    for (auto ballot : one_case.second) {
-      for (auto index : ballot) {
-        std::cout << index << ' ';
-      }
-
-      std::cout << '\n';
-    }
-  }
-}
-
+// 依然要计票，并且仍有票可计数。
 bool HasChoice(const Ballots& ballots) {
   for (const auto& one_vote: ballots) {
     if (!one_vote.empty()) {
@@ -98,87 +80,111 @@ bool HasChoice(const Ballots& ballots) {
   return false;
 }
 
-bool HasChoiceBesidesCandidates(const Ballots& ballots,
-                                const std::vector<int>& candidate_index) {
-  for (const auto& one_vote: ballots) {
-    if (!one_vote.empty() &&
-        std::find(candidate_index.cbegin(), candidate_index.cend(),
-                   one_vote.front()) == candidate_index.cend()) {
-      return true;
+// 剩下的候选人是否打平。
+bool IsCandidatesLeftTies(const std::set<size_t>& candidates_left,
+                          const std::vector<size_t>& least_votes_candidates) {
+  // 剩下候选人中有人不是得票最少的候选人之一。
+  for (const auto i : candidates_left) {
+    if (std::find(least_votes_candidates.cbegin(), least_votes_candidates.cend(), i)
+        == least_votes_candidates.cend()) {
+      return false;
     }
   }
 
-  return false;
+  return true;
 }
 
-std::vector<int> FindLeast(std::unordered_map<int, int>& vote) {
-  std::vector<int> least_indices;
-  if (!vote.empty()) {
-    int least = std::numeric_limits<int>::max();
-    for (const auto& one : vote) {
-      if (one.second < least) {
-        least_indices.clear();
-        least_indices.push_back(one.first);
-        least = one.second;
-      } else if (one.second == least) {
-        least_indices.push_back(one.first);
-      }
+// 找到得票最少的候选人的索引。
+std::vector<size_t> FindLeastVotesCandidatesIndex(std::unordered_map<size_t, int>& vote) {
+  std::vector<size_t> least_indices;
+  int least = std::numeric_limits<int>::max();
+  for (const auto& one : vote) {
+    if (one.second < least) {
+      least_indices.clear();
+      least_indices.push_back(one.first);
+      least = one.second;
+    } else if (one.second == least) {
+      least_indices.push_back(one.first);
     }
   }
 
   return least_indices;
 }
 
+void RemoveLeastVotesCandidates(std::set<size_t>& candidates_left,
+                                const std::vector<size_t>& least_votes_candidates) {
+  for (auto i : least_votes_candidates) {
+    candidates_left.erase(i);
+  }
+}
+
+std::vector<size_t> ProcessOneCaseReal(Ballots& ballots, const size_t candidates_num) {
+  // 剩下的候选人索引集合
+  std::set<size_t> candidates_left;
+  for (size_t i = 0; i < candidates_num; ++i) {
+    candidates_left.insert(i);
+  }
+
+  std::vector<size_t> result_one_case;
+  while (HasChoice(ballots) && result_one_case.empty()) {
+    std::unordered_map<size_t, int> vote_caculate;
+    // 对剩下候选人计票，避免未统计此轮没得票候选人
+    for (const auto index : candidates_left) {
+      vote_caculate[index] = 0;
+    }
+
+    int vote_total = 0;
+    for (const auto& one_ballot : ballots) {
+      if (!one_ballot.empty()) {
+        ++vote_caculate[one_ballot.front() - 1];
+        ++vote_total;
+      }
+    }
+
+    for (const auto& per_vote: vote_caculate) {
+      if (per_vote.second / vote_total > 0.5) {
+        result_one_case.push_back(per_vote.first);
+      }
+    }
+
+    if (result_one_case.empty()) {
+      std::vector<size_t> least_indices
+          = FindLeastVotesCandidatesIndex(vote_caculate);
+      if (IsCandidatesLeftTies(candidates_left, least_indices)) {
+        for (auto index : least_indices) {
+          result_one_case.push_back(index);
+        }
+      } else {
+        // 剔除得票最少的候选人 
+        RemoveLeastVotesCandidates(candidates_left, least_indices);
+        // 从选票池中删除所有已经出局的候选人的选票
+        for (auto& one_vote : ballots) {
+          one_vote.remove_if([&candidates_left](int x) {
+            return candidates_left.find(x-1) == candidates_left.end();
+          });
+        }
+      }
+    }
+  }
+
+  return result_one_case;
+}
+
+ResultOneCase ProcessOneCase(Ballots& ballots, const Candidates& candidates) {
+  ResultOneCase result_one_case;
+  std::vector<size_t> indices = ProcessOneCaseReal(ballots, candidates.size());
+  for (auto i : indices) {
+    result_one_case.push_back(candidates[i]);
+  }
+
+  return result_one_case;
+}
+
 Results Process(const Cases& cases) {
   Results results;
   for (OneCase one_case : cases) {
     auto& [candidates, ballots] = one_case;  // C++17 structured binding
-    ResultOneCase result_one_case;
-    while (HasChoice(ballots) && result_one_case.empty()) {
-      std::unordered_map<int, int> vote_caculate;
-      for (auto& one_ballot : ballots) {
-        if (!one_ballot.empty()) {
-          ++vote_caculate[one_ballot.front() - 1];
-        }
-      }
-
-      int vote_count = std::accumulate(
-          vote_caculate.begin(), vote_caculate.end(), 0,
-          [](int sum, const auto& a) {
-            return sum + a.second;
-          });
-      int smallest = vote_count;
-      for (const auto& per_vote: vote_caculate) {
-        if (per_vote.second / vote_count > 0.5) {
-          result_one_case.push_back(candidates[per_vote.first]);
-        }
-      }
-
-      if (result_one_case.empty()) {
-        std::vector<int> least_indices = FindLeast(vote_caculate);
-        if (HasChoiceBesidesCandidates(ballots, least_indices)) {
-          for (auto& one_vote : ballots) {
-            for (const auto index: least_indices) {
-              if (!one_vote.empty() && one_vote.front() - 1 == index) {
-                one_vote.pop_front();
-                break;
-              }
-            }
-          }
-        } else {
-          for (auto index : least_indices) {
-            result_one_case.push_back(candidates[index]);
-          }
-
-          for (auto& one_vote : ballots) {
-            if (!one_vote.empty()) {
-              one_vote.pop_front();
-            }
-          }
-        }
-      }
-    }
-
+    ResultOneCase result_one_case = ProcessOneCase(ballots, candidates);
     results.push_back(result_one_case);
   }
 
